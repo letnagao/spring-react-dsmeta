3f0fee690e091e4393cc4265d70a66fee2a8524c
letnagao <leticia-nagao@hotmail.com>
2022-07-17 16:03:34 -0300

    added useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import Header from './components/header'
-import SalesCard from './components/sales-card'
+import Header from './components/header';
+import SalesCard from './components/sales-card';
 
 function App() {
   return (
@@ -16,4 +16,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
--- frontend/src/components/sales-card/index.tsx
+++ frontend/src/components/sales-card/index.tsx
@@ -1,25 +1,32 @@
-import NotificationButton from '../notification-button'
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
-import './styles.css'
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import NotificationButton from '../notification-button';
+import 'react-datepicker/dist/react-datepicker.css';
+import './styles.css';
 
 function SalesCard() {
+  const min = new Date(new Date().setDate(new Date().getDate() - 365));
+  const max = new Date();
+
+  const [minDate, setMinDate] = useState(min);
+  const [maxDate, setMaxDate] = useState(max);
+
   return (
     <div className="dsmeta-card">
       <h2 className="dsmeta-sales-title">Vendas</h2>
       <div>
         <div className="dsmeta-form-control-container">
           <DatePicker
-            selected={new Date()}
-            onChange={(date: Date) => {}}
+            selected={minDate}
+            onChange={(date: Date) => setMinDate(date)}
             className="dsmeta-form-control"
             dateFormat="dd/MM/yyyy"
           />
         </div>
         <div className="dsmeta-form-control-container">
           <DatePicker
-            selected={new Date()}
-            onChange={(date: Date) => {}}
+            selected={maxDate}
+            onChange={(date: Date) => setMaxDate(date)}
             className="dsmeta-form-control"
             dateFormat="dd/MM/yyyy"
           />
@@ -86,4 +93,4 @@ function SalesCard() {
   )
 }
 
-export default SalesCard
+export default SalesCard;
