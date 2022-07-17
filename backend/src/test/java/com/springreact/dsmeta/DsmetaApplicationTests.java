04b5ff4aa45a27541c782295939aad44d477db91
letnagao <leticia-nagao@hotmail.com>
2022-07-17 14:34:26 -0300

    created project

---
+++ backend/src/test/java/com/springreact/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springreact.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
