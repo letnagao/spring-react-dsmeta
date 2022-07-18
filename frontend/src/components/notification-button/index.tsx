07b8c146f2a42321e0ee92ed81903414a61d3dda
letnagao <leticia-nagao@hotmail.com>
2022-07-17 23:27:11 -0300

    hotfix in sms integration

--- frontend/src/components/notification-button/index.tsx
+++ frontend/src/components/notification-button/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 }
 
 function handleClick(id: number) {
-  axios(`${BASE_URL}sales/${id}/notification`).then((response) => {
+  axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
     toast.info("SMS enviado com sucesso");
   })
 }
